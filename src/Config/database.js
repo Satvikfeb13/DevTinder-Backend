309499d132ced737fc2c72ca26e91b67a75e0f8c
satvikfeb13 <[psatvik96@gmail.com]>
2024-09-30 07:34:44 +0530

    get feed and user API

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const connectDB=async()=>{
     mongoose.connect(
-        "mongodb+srv://patilsatvik1324:0000000@namsatedev.arjq8.mongodb.net/"
+        "mongodb+srv://patilsatvik1324:0000000@namsatedev.arjq8.mongodb.net/DevTinder"
     )
 }
 module.exports= connectDB
--- src/app.js
+++ src/app.js
@@ -1,6 +1,46 @@
 const express= require("express");
 const connectDB=require("./Config/database.js")
 const app=express();
+const User=require("./models/user.js");
+app.use(express.json());
+app.post("/signup",async(req,res)=>{
+    const user= new User(req.body);
+    try{
+        await user.save();
+        res.send("user added successfully");
+    }catch(err){
+        res.status(400).send("Error saving the user:"+err.message);
+    }
+})
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    const user= await User.find({emailId:userEmail});
+    try{
+        if(user===0){
+            res.status(404).send("user not found");
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+
+        res.status(400).send("Something went wrong ");
+    }
+})
+app.get("/feed",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    const users=await User.findOne({emailId:userEmail});
+    try{
+        if(users===0){
+            res.status(401).send("Invalid user");
+        }else{
+            res.send(user);
+        }
+    }catch{
+        res.status(400).send("Something went wrong ");
+   
+    }
+
+})
 connectDB()
 .then(()=>{
     console.log("Database connect successfully ");
