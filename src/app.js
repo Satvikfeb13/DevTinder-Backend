352984a85e995b5240fdeecb383bcad81df51d52
satvikfeb13 <[psatvik96@gmail.com]>
2024-09-30 15:38:13 +0530

    update and Delete

--- src/app.js
+++ src/app.js
@@ -26,6 +26,21 @@ app.get("/user",async(req,res)=>{
         res.status(400).send("Something went wrong ");
     }
 })
+// get element by id     
+app.get("/byid",async(req,res)=>{
+    const userId=req.body.userId;
+    const user=await User.findById({_id:userId});
+    try{
+        if(!user){
+            res.status(401).send("user not found");
+        }else{
+            res.send(user);
+        }
+    }catch{
+        res.status(400).send("Something went wrong ");
+
+    }
+})
 app.get("/feed",async(req,res)=>{
     const userEmail=req.body.emailId;
     const users=await User.findOne({emailId:userEmail});
@@ -36,10 +51,31 @@ app.get("/feed",async(req,res)=>{
             res.send(user);
         }
     }catch{
-        res.status(400).send("Something went wrong ");
+        res.status(500).send("Something went wrong ");
    
     }
 
+})
+// delete user from database
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const user= await User.findByIdAndDelete({_id:userId});
+    try{
+        res.send("user deleted successfully");
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+// update user 
+app.patch("/user-update",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(400).send("something went wrong ");
+    }
 })
 connectDB()
 .then(()=>{
